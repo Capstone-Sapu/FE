fb467856820193b78d7918f7951c91cace92ad1d
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2023-12-11 20:53:30 +0800

    Membuat delay pada search agar tidak over request

--- src/components/elements/search/Search.jsx
+++ src/components/elements/search/Search.jsx
@@ -1,18 +1,33 @@
 /* eslint-disable react/prop-types */
 import { InputElement } from "../input";
-
-
+import { useState, useEffect } from "react";
 
 const Search = ({ searchValue, setSearchValue }) => {
+  const [inputValue, setInputValue] = useState(searchValue);
+
+  useEffect(() => {
+    const delay = 500; // Delay dalam milidetik
+
+    const timeoutId = setTimeout(() => {
+      setSearchValue(inputValue);
+    }, delay);
+
+    return () => clearTimeout(timeoutId);
+  }, [inputValue, setSearchValue]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
       <InputElement
         type="text"
-        value={searchValue}
+        value={inputValue}
         name="search"
         className="form-control"
         placeholder="Search"
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={handleInputChange}
       />
     </div>
   );
